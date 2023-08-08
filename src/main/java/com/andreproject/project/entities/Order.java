e525a3528dc47378b3a5fab8e48ae370b3970fc0
André Barcelos Michels de Mattos <112213284+abmMattos@users.noreply.github.com>
2023-08-08 14:54:52 -0300

    Payment, one-to-one association

--- src/main/java/com/andreproject/project/entities/Order.java
+++ src/main/java/com/andreproject/project/entities/Order.java
@@ -30,6 +30,8 @@ public class Order implements Serializable {
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
 
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
     public Order() {
     }
 
@@ -72,6 +74,14 @@ public class Order implements Serializable {
 
     public Set<OrderItem> getItems() {
         return items;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     @Override
